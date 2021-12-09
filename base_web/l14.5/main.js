b2484a29c35261a5d220f0f3eb19ad82bb37bdd3
Migeya <migeya@gmail.com>
2021-12-09 20:06:27 +0200

    lection14

---
+++ base_web/l14.5/main.js
@@ -0,0 +1,60 @@
+const myName2 = prompt("You main?")
+
+
+
+ 
+switch (myName2) {
+    case "Techies" :
+        alert(`You are gay`)
+        break;
+    case "Invoker" :
+        alert(`You choose wonderfull hero`)
+        break;
+    default:
+        alert(`You just main ${myName2}`)
+}
+
+
+
+
+
+// myName2 === "Storm" ? alert(`I also`) : alert(`why?`);
+
+// const number = 5;
+// const number2 = "5";
+// console.log(number == number2);
+
+
+
+// if (myName2 === "Roma") {
+//     alert("Hello admin Roma!")
+// } 
+// else if (myName2 === "Alex"){
+//     alert("You are Alex")
+// }
+// else {
+//     alert(myNumbers)
+// }
+
+
+
+
+// let myNumbers = 5;
+// console.log(myNumbers)
+
+
+// const myName ="Roma"
+// console.log(myName)
+
+
+// myNumbers = 10;
+// console.log(myNumbers)
+
+// const myBio = {
+//     myName: "Roma",
+//     myAge: 20,
+//     hasWife: false,
+//     myCar: undefined,
+//     hasDiamonds: null,
+// }
+// console.log(myBio)
